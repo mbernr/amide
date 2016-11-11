175f6e6ff27e45f2a6bc576805f145ed7e977d76
Michael Bernreiter <mbernreiter@hotmail.com>
2016-11-11 14:35:37 +0100

    konsultation geht prinzpiell

--- eCardDialog/KSE.cs
+++ eCardDialog/KSE.cs
@@ -18,25 +18,15 @@ namespace eCardDialog
             InitializeComponent();
 
             baseProperty[] prop = d.getBaseService().getFachgebieteByOrdId(d.getDialogId(), d.getOrdination()[0].ordinationId, d.getBereich().id);
-            Console.WriteLine("getFachgebieteByOrdId funktioniert");
-            //Console.ReadLine();
-/*
             KSERef.KseService kseService = new KSERef.KseService();
             kseService.Url = "https://10.196.4.114/kse/17";
             KSERef.behandlungsfallZusatzProperty[] behandlungsfall = kseService.getBehandlungsfaelleByFachgebietZusatz("01");
 
             KSERef.baseProperty[] behandlungsfallCode = kseService.getBehandlungsfaelle();
 
-            Console.WriteLine(behandlungsfall[0]);
-            //Console.ReadLine();
-
             svtProperty[] svtProp = d.getBaseService().getSVTs();
-            //Console.WriteLine(svtProp[0]);
-            //Console.ReadLine();
 
-            KSERef.ergebnisKonsultation ergebnisKonstultation = kseService.doKonsultation(eCard.cin, d.getDialogID(), eCard.nummer, svtProp[0].code, null, prop[0].code, behandlungsfallCode[0].code, null, null, true, true, d.getReader().id);
-            */
-            d.getBaseService().closeDialog(d.createDialog());
+            KSERef.ergebnisKonsultation ergebnisKonstultation = kseService.doKonsultation(eCard.cin, d.getDialogId(), eCard.nummer, svtProp[0].code, null, prop[0].code, behandlungsfallCode[0].code, null, null, true, true, d.getReader().id);
             
         }
     }
