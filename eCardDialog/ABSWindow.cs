d8c20dbd83d2523f80c2dc7b2b2b4a0ab382b568
Winni <winnomat@gmail.com>
2016-11-11 15:40:16 +0100

    ABS webservice, window

---
+++ eCardDialog/ABSWindow.cs
@@ -0,0 +1,21 @@
+﻿using eCardDialog.BaseRef;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eCardDialog
+{
+    public partial class ABSWindow : Form
+    {
+        public ABSWindow(Dialog d, card eCard)
+        {
+            InitializeComponent();
+        }
+    }
+}
