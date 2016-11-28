5ce5bacded5111df619be2dca95f02677f598a5b
Winni <winnomat@gmail.com>
2016-11-28 14:23:01 +0100

    ABS: sendenAnfrage

--- eCardDialog/ABSWindow.Designer.cs
+++ eCardDialog/ABSWindow.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.antragstyp = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(27, 103);
+            this.button1.Location = new System.Drawing.Point(238, 343);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(226, 116);
             this.button1.TabIndex = 0;
@@ -41,11 +42,20 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.sendenAnfrage_Click);
             // 
+            // antragstyp
+            // 
+            this.antragstyp.FormattingEnabled = true;
+            this.antragstyp.Location = new System.Drawing.Point(294, 163);
+            this.antragstyp.Name = "antragstyp";
+            this.antragstyp.Size = new System.Drawing.Size(121, 28);
+            this.antragstyp.TabIndex = 1;
+            // 
             // ABSWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.ClientSize = new System.Drawing.Size(720, 471);
+            this.Controls.Add(this.antragstyp);
             this.Controls.Add(this.button1);
             this.Name = "ABSWindow";
             this.Text = "ABSWindow";
@@ -56,5 +66,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox antragstyp;
     }
 }
--- eCardDialog/ABSWindow.cs
+++ eCardDialog/ABSWindow.cs
@@ -18,14 +18,8 @@ namespace eCardDialog
         private card eCard;
         private ABSRef.AbsService absService = new ABSRef.AbsService();
         private ABSRef.bewilligungsAnfrage bewilligungsAnfrage = new ABSRef.bewilligungsAnfrage();
-        //      bewilligungsAnfrage.setAntragstyp(java.lang.String antragstyp) 
-                    //ARZTBRIEF
-                    //Kennzeichnung einer Bewilligungsanfrage im Rahmen einer Arztbrieferstellung.
-                    //REZEPT
-                    //Kennzeichnung einer Bewilligungsanfrage im Rahmen einer Rezeptausstellung.
-        //      bewilligungsAnfrage.setPatientenDaten(PatientenDaten patientenDaten) 
-        //      bewilligungsAnfrage.setVerordnerinformation(java.lang.String verordnerinformation)
-                    //optional 
+        private ABSRef.anfrageAntwort anfrageAntwort = new ABSRef.anfrageAntwort();
+ 
         //      bewilligungsAnfrage.setVerordnungen(Verordnung[] verordnungen) 
 
         public ABSWindow(Dialog d, card eCard)
@@ -33,14 +27,55 @@ namespace eCardDialog
             InitializeComponent();
             this.d = d;
             this.eCard = eCard;
-            this.absService.Url = "https://10.196.4.114/abs/11";  
+            this.absService.Url = "https://10.196.4.114/abs/11";
+            antragstyp.Items.Add("ARZTBRIEF");
+            antragstyp.Items.Add("REZEPT");
+            this.bewilligungsAnfrage.patientenDaten = this.createPatientenDaten(eCard);
+            var verordnungen = new List<ABSRef.verordnung>();
+            verordnungen.Add(this.createVerordnung());
+            this.bewilligungsAnfrage.verordnungen = verordnungen.ToArray();
+        }
+        
+        private ABSRef.patientenDaten createPatientenDaten(card eCard)
+        {
+            ABSRef.patientenDaten patientenDaten = new ABSRef.patientenDaten();
+            patientenDaten.SVNummer = eCard.nummer;
+            patientenDaten.nachname = eCard.nachname;
+            patientenDaten.vorname = eCard.vorname;
+            patientenDaten.geschlecht = eCard.geschlechtCode;
+            patientenDaten.rezeptGebBefreit = true;
+            patientenDaten.druckNachname = eCard.nachname;
+            patientenDaten.druckVorname = eCard.vorname;
+           
+            return patientenDaten;
+        }
+
+        private ABSRef.verordnung createVerordnung()
+        {
+            ABSRef.verordnung verordnung = new ABSRef.verordnung();
+            ABSRef.medikament medikament = new ABSRef.medikament();
+            medikament.name = "Axt";
+            verordnung.medikament = medikament;
+            verordnung.packungsanzahl = 5;
+            verordnung.diagnose = "knie an'ghaut :(";
+            verordnung.begruendung = "pfft";
+            
+            return verordnung;
         }
 
         private void sendenAnfrage_Click(object sender, EventArgs e)
         {
+            if (antragstyp.SelectedItem == null)
+            {
+                return;
+            }
+
+            this.bewilligungsAnfrage.antragstyp = antragstyp.SelectedItem.ToString();
+
             try
             {
-                this.absService.sendenAnfrage(this.d.getDialogId(), this.bewilligungsAnfrage, this.d.getReader().id, null);
+                this.anfrageAntwort =  this.absService.sendenAnfrage(this.d.getDialogId(), this.bewilligungsAnfrage, this.d.getReader().id, null);
+                Console.WriteLine("ABS Erfolg");
             }
             catch (Exception exception)
             {
